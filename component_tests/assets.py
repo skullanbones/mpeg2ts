b8f323ba988d82406fbf83e5da239b8f8d0d1459
Christian Köhnenkamp <cvk5@me.com>
2018-06-22 10:30:22 +0200

    [#45] Fix codacy issue. Use one class for downloading everything.

--- component_tests/assets.py
+++ component_tests/assets.py
@@ -1,23 +1,8 @@
 import xmltodict
-import urllib.request
 import subprocess
 
 BASE_URL = "https://s3-us-west-2.amazonaws.com/tslibteststreams"
 
-file  = urllib.request.urlopen(BASE_URL)
-data = file.read()
-file.close()
-data = xmltodict.parse(data)
-contents = data['ListBucketResult']['Contents']
-
-# find each asset and append to list
-assets = []
-for asset in contents:
-    assets.append(asset['Key'])
-
-print(assets)
-
-#############
 class Downloader():
     def __init__(self, timeout = "20", chunk_size = "0-10000000", download_dir="downloaded_files"):
         self.timeout = timeout
@@ -33,7 +18,7 @@ class Downloader():
             return False
 
 
-    def curl_dl(self,url, file_name):
+    def curl_dl(self, url, file_name):
         # TODO Check curl command exists
         output = self.download_dir + "/" + file_name
         command=["curl", url, "--range", self.chunk_size,"--output", output,  "--create-dirs", "--connect-timeout", self.timeout]
@@ -41,9 +26,23 @@ class Downloader():
             download_state=subprocess.call(command)
         except Exception as e:
             print(e)
-        #if download_state==0 => successfull download
         return download_state
 
 
-test = Downloader()
-test.download_file(BASE_URL + assets[0])
+dwnl = Downloader()
+
+# 1. Download xml
+dwnl.download_file(BASE_URL, "assets.xml")
+
+# 2. Parse asset list
+assets = []
+with open('downloaded_files/assets.xml') as fd:
+    data = xmltodict.parse(fd.read())
+    contents = data['ListBucketResult']['Contents']
+
+    for asset in contents:
+        assets.append(asset['Key'])
+print(assets)
+
+# 3. Download assets
+dwnl.download_file(BASE_URL + "/" + assets[0], "asset0.m2ts")
