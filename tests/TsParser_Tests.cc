49b7f0aae92c4b00a355fd6fcbbee12528b99585
Christian Köhnenkamp <cvk5@me.com>
2018-07-23 17:49:47 +0200

    Adding TsDemuxer unit tests

--- tests/TsParser_Tests.cc
+++ tests/TsParser_Tests.cc
@@ -360,5 +360,6 @@ TEST(TsParserTests, TestParsePcr)
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
