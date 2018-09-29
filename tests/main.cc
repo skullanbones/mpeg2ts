aedb72648e743ffb02a0ad5b0415513877aa934b
Christian Köhnenkamp <cvk5@me.com>
2018-09-29 17:07:27 +0200

    Fix Makefile Linux gtest
    Add main.cc file for gtest

--- tests/main.cc
+++ tests/main.cc
@@ -1,9 +1,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
+    //system("PAUSE");
+    //return 0;
 }
