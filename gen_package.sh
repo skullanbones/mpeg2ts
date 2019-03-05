855db9d53d10720fbd59b025adb4c6284c08273f
Christian Köhnenkamp <cvk5@me.com>
2019-03-05 22:02:13 +0100

    Package only shared libs

--- gen_package.sh
+++ gen_package.sh
@@ -1,48 +1,31 @@
 #!/bin/bash
 # Script to package both debug and release build with CPack
 
-echo "1. Create debug/ release/ folders for static and shared libraries"
+echo "1. Create debug/ release/ folders for shared libraries"
 
-build_types=(debug_static release_static debug_shared release_shared)
+build_types=(Debug Release)
 
 for i in ${build_types[@]}; do
     echo mkdir: $i
     mkdir $i
 done
 
+echo "2. Configure CMake"
+for type in ${build_types[@]}; do
+    echo "Configure and build / $type shared lib"
+    cd $type
+    cmake -DCMAKE_BUILD_TYPE=$type -DBUILD_SHARED_LIBS=YES ..
+    make -j $(nproc)
+    cd ..
+done
 
-echo "2 .Configure and build /debug static lib"
-cd debug_static
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install ..
-make -j $(nproc)
-cd ..
 
-echo "3 .Configure and build /debug shared lib"
-cd debug_shared
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DBUILD_SHARED_LIBS=YES ..
-make -j $(nproc)
-cd ..
-
-echo "4. Configure and build /release static lib"
-cd release_static
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install ..
-make -j $(nproc)
-cd ..
-
-echo "5. Configure and build /release shared lib"
-cd release_shared
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DBUILD_SHARED_LIBS=YES ..
-make -j $(nproc)
-cd ..
-
-echo "6. Create CPackConfig.cmake"
-echo 'include("release_static/CPackConfig.cmake")
+echo "3. Create CPackConfig.cmake"
+echo 'include("Release/CPackConfig.cmake")
 set(CPACK_INSTALL_CMAKE_PROJECTS
-    "debug_static;mpeg2ts;ALL;/"
-    "release_static;mpeg2ts;ALL;/"
-    "debug_shared;mpeg2ts;ALL;/"
-    "release_shared;mpeg2ts;ALL;/"
+    "Debug;mpeg2ts;ALL;/"
+    "Release;mpeg2ts;ALL;/"
 )' > CPackConfig.cmake
 
-echo "7. Generate package"
+echo "4. Generate package"
 cpack --config CPackConfig.cmake
