43edb3f6c5de93c5e9c5eb5e524422f1e4d5ba1d
Christian Köhnenkamp <cvk5@me.com>
2019-05-02 21:28:46 +0200

    Add doxygen

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.12)
+
+# Require dot, treat the other components as optional
+find_package(
+    Doxygen 
+    OPTIONAL_COMPONENTS dot mscgen dia
+)
+
+message("test:" ${PROJECT_SOURCE_DIR})
+
+if(NOT DOXYGEN_FOUND)
+    message(WARNING "Doxygen not found on your system. Bailing out...")
+else()
+    doxygen_add_docs(
+        doxygen
+        ${PROJECT_SOURCE_DIR}/src ${PROJECT_SOURCE_DIR}/tests ${PROJECT_SOURCE_DIR}/bench ${PROJECT_SOURCE_DIR}/include ${PROJECT_SOURCE_DIR}/samples
+        COMMENT "Generate doxygen pages"
+    )
+endif()
