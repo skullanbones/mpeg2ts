8d3cb74ab80a5d85b372f00e1b437444d642b908
Christian Köhnenkamp <cvk5@me.com>
2019-05-03 10:26:34 +0200

    Minor doxygen cmake update

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -8,7 +8,10 @@ find_package(
 
 if(NOT DOXYGEN_FOUND)
     message(WARNING "Doxygen not found on your system. Bailing out...")
+    return()
 else()
+    message(STATUS "doxygen found: ${DOXYGEN_EXECUTABLE}")
+
     doxygen_add_docs(
         doxygen
         ${PROJECT_SOURCE_DIR}/README.md ${PROJECT_SOURCE_DIR}/src ${PROJECT_SOURCE_DIR}/include ${PROJECT_SOURCE_DIR}/samples
