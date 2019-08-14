5699909780cbea855f2de2f02f915276b2856353
Christian Köhnenkamp <cvk5@me.com>
2019-08-14 17:56:57 +0200

    Make buildem.sh work again...

--- apps/WebAssembly/buildem.sh
+++ apps/WebAssembly/buildem.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
+
+ROOT_FOLDER=$(pwd)/../..
+APP_FOLDER=$ROOT_FOLDER/apps/WebAssembly
+LIB_FOLDER=$ROOT_FOLDER/libs
+
 em++ -std=c++11 -O3 -s WASM=1 -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 -s ALLOW_MEMORY_GROWTH=1 -s \
 "EXTRA_EXPORTED_RUNTIME_METHODS=['cwrap', 'ccall']" \
--I include/mpeg2vid/ \
--I include/h264 \
--I src \
--I build \
--I include \
--I 3rd-party/plog-1.1.4/include \
--I 3rd-party/json-3.5.0/include \
-WebAssembly/main.cc \
-src/h264/H264Parser.cc \
-src/GetBits.cc \
-src/TsStatistics.cc \
-src/TsParser.cc \
-src/TsDemuxer.cc \
-src/mpeg2vid/Mpeg2VideoParser.cc \
-src/mpeg2vid/Mpeg2Codec.cc \
-src/PsiTables.cc
-
+-I $LIB_FOLDER/mpeg2codec/include \
+-I $LIB_FOLDER/h264codec/include \
+-I $LIB_FOLDER/mpeg2codec/src \
+-I $LIB_FOLDER/h264codec/src \
+-I $LIB_FOLDER/mpeg2ts/src \
+-I $ROOT_FOLDER/build/libs/mpeg2ts \
+-I $LIB_FOLDER/mpeg2ts/include \
+-I $ROOT_FOLDER/3rd-party/plog-1.1.4/include \
+-I $ROOT_FOLDER/3rd-party/json-3.5.0/include \
+$APP_FOLDER/src/main.cc \
+$LIB_FOLDER/mpeg2ts/src/TsStatistics.cc \
+$LIB_FOLDER/mpeg2ts/src/TsParser.cc \
+$LIB_FOLDER/mpeg2ts/src/TsDemuxer.cc \
+$LIB_FOLDER/mpeg2ts/src/GetBits.cc \
+$LIB_FOLDER/mpeg2ts/src/PsiTables.cc \
+$LIB_FOLDER/h264codec/src/H264Parser.cc \
+$LIB_FOLDER/mpeg2codec/src/Mpeg2VideoParser.cc \
+$LIB_FOLDER/mpeg2codec/src/Mpeg2Codec.cc \
